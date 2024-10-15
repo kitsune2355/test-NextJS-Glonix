d579ffde80c0d158625e7bd981b6749499dcf16c
pemika.b <pemika.b@avlgb.com>
2024-10-15 23:28:05 +0700

    Add Container

---
+++ app/components/Container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className="mx-auto max-w-7xl py-8 sm:px-12 lg:px-8">{children}</div>
+  );
+}
