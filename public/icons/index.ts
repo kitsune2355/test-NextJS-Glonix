4fbe3ca3e55c8b74f5e26c911ef697ed1e2a384d
pemika.b <pemika.b@avlgb.com>
2024-10-15 23:25:09 +0700

    Add icon & image

---
+++ public/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Facebook } from "./facebook.svg";
+export { default as Instagram } from "./instagram.svg";
+export { default as LinkedIn } from "./linkedin.svg";
+export { default as Twitter } from "./twitter.svg";
+export { default as YouTube } from "./youtube.svg";
